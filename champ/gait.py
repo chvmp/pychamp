56483df745d156f4266754bed7804f9a182c712c
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-05-14 19:50:31 +0800

    set fgait signals to 0.5 before first touchdown to prevent stance/swing to aggressively move legs at start time

--- champ/gait.py
+++ champ/gait.py
@@ -1,19 +1,21 @@
 import time
 import numpy as np
 
-SECONDS_TO_MILLIS = 1000.0
+SECONDS_TO_MICROS = 1000000.0
 
 class TDEvent(object):
-    def __init__(self, base, stance_duration=0.25, swing_duration=0.25):
-        self._swing_phase_period = swing_duration * SECONDS_TO_MILLIS
-        self._stance_phase_period = stance_duration  * SECONDS_TO_MILLIS
+    def __init__(self, stance_duration=0.25, swing_duration=0.25):
+        self._swing_phase_period = swing_duration * SECONDS_TO_MICROS
+        self._stance_phase_period = stance_duration  * SECONDS_TO_MICROS
         self._stride_period = self._stance_phase_period + self._swing_phase_period
         
         self._elapsed_time_ref = 0.0
-        self._last_touch_down = 0.0
+        self._last_touch_down = int(round(time.perf_counter() * 1000000))
         self._leg_clocks = np.zeros((4,1))
         self._stance_phase_signal =  np.zeros((4,1))
         self._swing_phase_signal = np.zeros((4,1))
+
+        self._has_swung = False
 
         self._phase_delay_trot= np.array([[0.0], [0.5], [0.5], [0.0]])
         self._phase_delay_zero = np.array([[0.5], [0.0], [0.0], [0.5]])
@@ -23,10 +25,12 @@ class TDEvent(object):
 
     def run(self, step_length, time):
         if step_length == 0.0:
+            self._has_swung = False
             return self._stance_phase_signal, self._swing_phase_signal
    
         if time - self._last_touch_down >= self._stride_period:
             self._last_touch_down = time
+
         # elif self._init_transit_time == 0.0:
         #     self._init_transit_time = time
         # elif self._last_touch_down == 0:  
@@ -39,7 +43,7 @@ class TDEvent(object):
         #                    1)
         #     self._phase_delay = self._phase_delay_zero + (self._delta_delay * sat)
         #     print(self._phase_delay)
-
+        
         if self._elapsed_time_ref > self._stride_period:
             self._elapsed_time_ref = self._stride_period
         else:
@@ -58,5 +62,13 @@ class TDEvent(object):
         self._swing_phase_signal = np.where((self._leg_clocks > self._stance_phase_period) & (self._leg_clocks < self._stride_period), 
                                             (self._leg_clocks - self._stance_phase_period) / self._swing_phase_period,
                                              self._swing_phase_signal)
+
+        if not self._has_swung and self._stance_phase_signal[0,0] < 0.5:
+            self._stance_phase_signal[0,0] = 0.5
+            self._stance_phase_signal[3,0] = 0.5
+            self._swing_phase_signal[1,0] = 0.5
+            self._swing_phase_signal[2,0] = 0.5
+        else:
+            self._has_swung = True
         
-        return self._stance_phase_signal, self._swing_phase_signal
+        return self._swing_phase_signal, self._stance_phase_signal
