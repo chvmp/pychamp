b110ec557582d9243998691638bba95df782bc49
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-05-19 18:55:33 +0800

    added base sensor and foot contact states

--- champ/pybullet/sensors.py
+++ champ/pybullet/sensors.py
@@ -1,13 +1,15 @@
 import pybullet as p
+import numpy as np
 
 class PyBulletSensors:
-    def __init__(self, bullet_id, joint_names, link_names):
-        self._bullet_id = bullet_id
+    def __init__(self, plane_id, base_id, joint_names, link_names):
+        self._base_id = base_id
+        self._plane_id = plane_id
         self._joint_ids = [0] * 16
         self._actuator_ids = [0]  * 12  
         self._foot_ids = [0] * 4 
 
-        num_urdf_joints = p.getNumJoints(bullet_id)
+        num_urdf_joints = p.getNumJoints(base_id)
         foot_idx = [3, 7, 11, 15]
 
         actuator_names = []
@@ -20,7 +22,7 @@ class PyBulletSensors:
                 actuator_names.append(joint_name)
 
         for i in range(num_urdf_joints):
-            joint_info = p.getJointInfo(bullet_id, i)
+            joint_info = p.getJointInfo(base_id, i)
             joint_name = joint_info[1]
             joint_name = joint_name.decode("utf-8")
             
@@ -34,8 +36,34 @@ class PyBulletSensors:
     def actuator_ids(self):
         return self._actuator_ids         
         
-    def get_joint_positions(self):
-        joint_states = p.getJointStates(self._bullet_id, self._actuator_ids)
+    @property
+    def foot_ids(self):
+       return self._foot_ids
+
+    def joint_positions(self):
+        joint_states = p.getJointStates(self._base_id, self._actuator_ids)
         return [joint_info[0] for joint_info in joint_states]
 
-   
+    def contact_states(self):
+        contact_info = p.getContactPoints(self._base_id, self._plane_id)
+        foot_contacts = np.zeros(([4,1]))
+
+        for single_contact in contact_info:
+            link_id = single_contact[3]
+            if link_id in self._foot_ids:
+                foot_contacts[self._foot_ids.index(link_id), :] = 1
+        
+        return foot_contacts
+
+    def base_velocity(self):
+        return p.getBaseVelocity(self._base_id)
+
+    def base_position(self):
+        return p.getBasePositionAndOrientation(self._base_id)[0]
+
+    def base_orientation_rpy(self):
+        quat = p.getBasePositionAndOrientation(self._base_id)[1]
+        return p.getEulerFromQuaternion(quat)
+
+    def base_orientation_quat(self):
+        return p.getBasePositionAndOrientation(self._base_id)[1]
