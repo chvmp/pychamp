0c8ac4946eecf03b6726b79754f37392cc472de5
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-05-22 01:02:55 +0800

    cleaned up

--- examples/sim.py
+++ examples/sim.py
@@ -24,10 +24,10 @@ import champ.pybullet.utils as pb_utils
 class Champ:
     def __init__(self):
         #change to the robot you want to use ie.
-        robot_profile = spot
+        # robot_profile = spot
         # robot_profile = anymal_b
         # robot_profile = anymal_c
-        # robot_profile = open_quadruped
+        robot_profile = open_quadruped
 
         physics_client = p.connect(p.GUI)
         p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 0)
