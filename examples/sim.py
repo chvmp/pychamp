b0abb09bdac37a06f5a3b538692eb5639d7d9d84
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-05-22 14:38:47 +0800

    added sensors example

--- examples/sim.py
+++ examples/sim.py
@@ -20,6 +20,7 @@ from champ.robots.profile import SpotMicro as spotmicro
 from champ.robots.profile import StochLite as stochlite
 from champ.pybullet.sensors import PyBulletSensors
 import champ.pybullet.utils as pb_utils
+import champ.geometry as geom
 
 class Champ:
     def __init__(self):
@@ -55,6 +56,8 @@ class Champ:
         for i in range(p.getNumJoints(base_id)):
             joint_info = p.getJointInfo(base_id, i)
 
+
+        req_pose.orientation.roll = 0.3
         while True:
             req_vel = pb_utils.get_req_vel(base_id, req_vel)
        
@@ -62,9 +65,33 @@ class Champ:
             target_joint_positions = ik.inverse(foot_positions)
 
             quadruped.joint_positions = sensors.joint_positions()
+
+            base_orientation = sensors.base_orientation_quat()
+            base_rot_matrix = geom.quat_to_matrix(
+                x=base_orientation[0],
+                y=base_orientation[1],
+                z=base_orientation[2],
+                w=base_orientation[3]
+            )
+ 
+            base_orientation = sensors.base_orientation_rpy()
+            base_rot_matrix = geom.rpy_to_matrix(
+                roll=base_orientation[0],
+                pitch=base_orientation[1],
+                yaw=base_orientation[2]
+            )
+     
             foot_positions_from_base = quadruped.feet.position
             foot_positions_from_hip = quadruped.transform_to_hip(foot_positions_from_base)
-           
+            
+            linear, angular = sensors.base_velocity()
+            l_vx, l_vy, l_vz = linear
+            a_vx, a_vy, a_vz = angular
+
+            world_x, world_y, world_z = sensors.base_position()
+
+            contact_states = sensors.contact_states()
+
             p.setJointMotorControlArray(base_id, sensors.actuator_ids, p.POSITION_CONTROL, list(target_joint_positions))
 
         p.disconnect()
